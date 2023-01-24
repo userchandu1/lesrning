078b79ebbc6726cbb382c71ecfc31e12de656755
Chandan <chandanjume@gmail.com>
2023-01-24 08:18:27 -0800

    commit on new branch

--- api/models.py
+++ api/models.py
@@ -6,3 +6,4 @@ class Comment(models.Model):
     email = models.EmailField()
     content = models.CharField(max_length=200)
     created = models.DateTimeField(default=datetime.now())
+    name = models.TextField()
