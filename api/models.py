94e18adec67687f729ffd70d22b74b0d1f884a36
Chandan <chandanjume@gmail.com>
2023-01-25 06:55:07 -0800

    migreation file

--- api/models.py
+++ api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 class Comment(models.Model):
     email = models.EmailField()
     content = models.CharField(max_length=200)
-    created = models.DateTimeField(default=datetime.now())
+    created = models.DateTimeField(auto_now_add=True)
     name = models.TextField()
-    address1 = models.CharField(max_length=200)
+    address1 = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.email
