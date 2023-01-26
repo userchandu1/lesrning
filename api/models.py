3016a8e0008ff7a1fce0185b04997421ce0f680d
Chandan <chandanjume@gmail.com>
2023-01-25 23:33:23 -0800

    commited after migrations

--- api/models.py
+++ api/models.py
@@ -1,13 +1,44 @@
 from django.db import models
-from datetime import datetime, timezone
+from datetime import datetime, timezone, date
 
 
 class Comment(models.Model):
     email = models.EmailField()
     content = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(datetime.now())
     name = models.TextField()
     address1 = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.email
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField(default=date.today)
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField(default=0)
+    number_of_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.headline
